b1ba97426a8b648b78145a4e34bf8562bf66506d
DelSquared <37172200+DelSquared@users.noreply.github.com>
2019-01-12 00:26:42 +0100

    Update main.py

--- python/main.py
+++ python/main.py
@@ -1,9 +1,9 @@
 import numpy as np 
 import matplotlib.pyplot as plt 
 
-ln2=np.log(2) #hlflife is fixed at 1 for simplicity
+ln2=np.log(2) #halflife is fixed at 1 for simplicity
 
-sims = 10
+sims = 1000
 
 No = 100
 days = 20
@@ -23,7 +23,12 @@ for _ in range(sims):
   theoretical = No*np.exp(-ln2*t)
 
   fig=plt.figure()
-  plt.plot(t,count)
-  plt.plot(t,theoretical)
+  plt.plot(t,count,label="Simulation")
+  plt.plot(t,theoretical,label="Theoretical")
+  plt.title("Plot of N against t (Theoretical vs Simulation of Radioactive Decay)")
+  plt.xlabel("t")
+  plt.ylabel("N")
+  plt.legend()
   plt.grid()
   plt.savefig("plot {}.png".format(_+1))
+  plt.close(fig)
